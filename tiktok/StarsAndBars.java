b86badb60a1e5f2abdc2bcd71801c9d47f4dd88d
Raj Shah <rshah62@uncc.edu>
2022-02-06 18:38:54 -0500

    Create StarsAndBars.java

---
+++ tiktok/StarsAndBars.java
@@ -0,0 +1,53 @@
+public static List<Integer> starsAndBars(String strToEvaluate,List<Integer> startIndex,List<Integer> endIndex)
+{
+List<Integer> out=new ArrayList<Integer>();
+for(int i=0;i<startIndex.size();i++)
+{
+//Get the two Indices into start,end
+int x=startIndex.get(i);
+int y=endIndex.get(i);
+  
+//BarsCount to know if we have found two bars or not
+  
+int startBarIndex=-1,endBarIndex=-1;
+for(int j=x-1;j<y;j++)
+{
+if(strToEvaluate.charAt(j)=='|')
+{
+startBarIndex=j;
+break;
+}
+}
+for(int j=y-1;j>=x-1;j--)
+{
+if(strToEvaluate.charAt(j)=='|')
+{
+endBarIndex=j;
+break;
+}
+}
+
+if(startBarIndex==-1 || endBarIndex==-1 || startBarIndex==endBarIndex)
+{
+//No Stars are there between any 2 bars in the given range
+out.add(0);
+}
+else
+{
+//Count the bars
+int barsCount=0;
+for(int j=x-1;j<y;j++)
+{
+if(strToEvaluate.charAt(j)=='|')
+{
+barsCount++;
+}
+}
+  
+int starsCount=(endBarIndex-startBarIndex+1)-barsCount ;
+// System.out.println(startBarIndex+"-->"+endBarIndex+"-->"+barsCount);
+out.add(starsCount);
+}
+}
+return out;
+}
