e0b721ebc48ee0df3a67533ee707a2842f87ac15
Raj Shah <rshah62@uncc.edu>
2022-02-06 18:46:30 -0500

    Create Aladin.java

---
+++ tiktok/Aladin.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+	int[] magic = {2,4,5,2};
+	int[] dist = {4,3,1,3};
+	System.out.println(getStartPoint(magic, dist));
+}
+
+private static int getStartPoint(int[] magic, int[] dist) {
+	int s = 0, r = 0, d = 0;
+    for(int i=0;i<magic.length;i++){
+        r += magic[i] - dist[i];
+        if(r < 0){
+            s = i+1;
+            d += r;
+            r = 0;
+        }
+    }
+    return d + r >= 0 ? s : -1;
+}
